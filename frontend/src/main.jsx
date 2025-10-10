6daecdc33b309460619a8e4ca1e304d731becfe9
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-10-10 20:05:04 +0530

    Minor changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CONFIG from './services/config'
+import { CONFIG } from './services/config'
 
 const link = document.createElement('link')
 link.rel = 'manifest'
